791053ceca2eb92ac1b874cb43e69db7b64bc8f4
Linlccc <1610450962@qq.com>
2022-04-08 18:16:36 +0800

    框架基本文件架构，基本脚本

---
+++ framework/Easy.Extensions.DependencyInjection.Abstractions/src/Easy.Extensions.DependencyInjection.Abstractions/InjectAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace Easy.Extensions.DependencyInjection.Abstractions;
+
+/// <summary>
+/// 自动注入
+/// </summary>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+public class InjectAttribute : Attribute
+{
+    public InjectAttribute() { }
+
+    /// <summary>
+    /// 要求
+    /// <br>如果没有值会抛出异常</br>
+    /// </summary>
+    public bool Require { get; set; }
+
+    /// <summary>
+    /// 服务key
+    /// <br>获取服务时，会通过key获取</br>
+    /// </summary>
+    public string? Key { get; set; }
+}
+
