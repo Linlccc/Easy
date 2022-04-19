69cfd0e9bee2d78b14459f8fb20e7d728e3260be
Linlccc <1610450962@qq.com>
2022-04-19 19:40:24 +0800

    添加合并json工具

---
+++ framework/Easy.Tool.MergeJson/src/MergeJson.cs
@@ -0,0 +1,19 @@
+﻿using Task = Microsoft.Build.Utilities.Task;
+
+namespace Easy.Tool.MergeJson;
+
+/// <summary>
+/// 实现合并json任务
+/// </summary>
+public class MergeJson : Task
+{
+    
+    /// <summary>
+    /// msbuild 执行方法
+    /// </summary>
+    /// <returns>返回true表示任务成功，返回false表示任务失败</returns>
+    public override bool Execute()
+    {
+        return false;
+    }
+}
