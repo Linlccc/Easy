7adf700cd1d3474e127c34b7b456ece00d8587b2
Linlccc <1610450962@qq.com>
2022-04-21 20:37:12 +0800

    完成基础json合并

--- framework/Easy.Tool.MergeJson/src/MergeJson.cs
+++ framework/Easy.Tool.MergeJson/src/MergeJson.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
+﻿using System.Collections.Concurrent;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Extensions.Configuration;
 using Task = Microsoft.Build.Utilities.Task;
 
 namespace Easy.Tool.MergeJson;
@@ -13,7 +15,7 @@ public class MergeJson : Task
     /// 主要json文件
     /// </summary>
     [Required]
-    public string MainJsonFiles { get; set; }
+    public ITaskItem[] MainJsonFiles { get; set; }
 
     /// <summary>
     /// 需要合并的json文件
@@ -34,27 +36,65 @@ public class MergeJson : Task
     public ITaskItem[] ParticipateMergeJsonFiles { get; set; }
 
     /// <summary>
-    /// 文件路径集合
-    /// </summary>
-    private List<string> JsonFilePaths => JsonFileItems.Select(j => j.GetMetadata("FullPath")).ToList();
-
-    /// <summary>
     /// msbuild 执行方法
     /// </summary>
     /// <returns>返回true表示任务成功，返回false表示任务失败</returns>
     public override bool Execute()
     {
-        // 可用 Directory.GetCurrentDirectory()
-        Directory.GetParent(AppContext.BaseDirectory).GetFiles(MainJsonFiles, SearchOption.TopDirectoryOnly);
+        if (!Directory.Exists(OutputPath)) Directory.CreateDirectory(OutputPath);
+        // 确认参与合并的json文件
+        ConcurrentDictionary<string, ITaskItem> participateMergeJsonFiles = new();
+        // 得到所有主文件路径
+        List<(string Path, ITaskItem Item)> mainFiles = MainJsonFiles.Select(m => (m.GetMetadata("FullPath"), m)).ToList();
+        // 得到所有可能参加合并的文件
+        List<(string Path, ITaskItem Item)> jsonFiles = JsonFileItems.Select(m => (m.GetMetadata("FullPath"), m)).ToList();
+        try
+        {
+            foreach ((string Path, ITaskItem Item) in mainFiles)
+            {
+                // 如果没有该文件跳过
+                if (!jsonFiles.Any(j => j.Path == Path)) continue;
 
-        System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        string value = File.ReadAllText(MainJsonFiles);
-        File.WriteAllText(OutputPath+"A.G.S.json", value+"\r\n"+ JsonFilePaths[0].GetType().FullName + "\r\n" + AppContext.BaseDirectory + "\r\n" +
-            Directory.GetCurrentDirectory());
+                // 标记当前json已经参与合并
+                participateMergeJsonFiles.TryAdd(Path, Item);
 
-        ParticipateMergeJsonFiles = JsonFileItems;
+                // 加载json配置
+                IConfigurationRoot json = new ConfigurationBuilder().AddJsonFile(Path).Build();
 
+                // 得到要合并json的子目录
+                string[] subDirectorys = json.GetSection(Item.GetMetadata("SubDirectoryNode")).Get<string[]>()?.Select(sd => sd.EndsWith("\\") ? sd : sd + "\\").Where(sd => Directory.Exists(sd)).ToArray() ?? Array.Empty<string>();
+                // 得到要排除的子文件(文件名)节点
+                string[] excludeSubFiles = json.GetSection(Item.GetMetadata("ExcludeSubFilesNode")).Get<string[]>() ?? Array.Empty<string>();
 
-        return true;
+                // 得到所有要合并的文件
+                List<FileInfo> subFiles = new();
+                foreach (string subDir in subDirectorys) subFiles.AddRange(Directory.GetParent(subDir).GetFiles("*.json"));
+                subFiles = subFiles.Where(sf => !excludeSubFiles.Any(esf => esf == sf.Name)).ToList();
+                IEnumerable<(string Path, ITaskItem Item)> mergeJsonFiles = jsonFiles.Where(jf => subFiles.Any(sf => sf.FullName == jf.Path));
+
+                // 得到合并json
+                StringBuilder stringBuilder = new("{");
+                stringBuilder.AppendLine(File.ReadAllText(Path).Trim().Trim('{', '}'));
+                foreach ((string mergePath, ITaskItem mergeItem) in mergeJsonFiles)
+                {
+                    stringBuilder.AppendLine(File.ReadAllText(mergePath).Trim().Trim('{', '}'));
+                    participateMergeJsonFiles.TryAdd(mergePath, mergeItem);
+                }
+                stringBuilder.Append("}");
+
+                // 写入文件
+                File.WriteAllText(OutputPath + Item.GetMetadata("Filename") + Item.GetMetadata("Extension"), stringBuilder.ToString());
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.LogErrorFromException(ex);
+        }
+        finally
+        {
+            ParticipateMergeJsonFiles = participateMergeJsonFiles.Values.ToArray();
+        }
+        // 有错误自动返回任务失败
+        return !Log.HasLoggedErrors;
     }
 }
--- framework/Easy.Tool.MergeJson/tests/Easy.Tool.MergeJson.Test/MergeJsonTest.cs
+++ framework/Easy.Tool.MergeJson/tests/Easy.Tool.MergeJson.Test/MergeJsonTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 using Moq;
 using Xunit;
 
@@ -13,25 +14,49 @@ public class MergeJsonTest
     private Mock<IBuildEngine> buildEngine;
     private List<BuildErrorEventArgs> errors;
 #pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
+    private string deaultPath;
+
+    private ITaskItem[] MainJsonFiles;
+    private ITaskItem[] JsonFileItems;
 
     public MergeJsonTest()
     {
         buildEngine = new Mock<IBuildEngine>();
         errors = new List<BuildErrorEventArgs>();
+        // 得到主要json文件
+        MainJsonFiles = new[]
+        {
+            new TaskItem("appsettings.json"),
+            new TaskItem("appsettings.Development.json"),
+            new TaskItem("appsettings.1.json"),
+        };
+        foreach (var item in MainJsonFiles)
+        {
+            item.SetMetadata("SubDirectoryNode", "CustomConfigInfo:ConfigFileFolders");
+            item.SetMetadata("ExcludeSubFilesNode", "CustomConfigInfo:ExcludeSubFiles");
+        }
+
+        // 得到所有json文件
+        List<ITaskItem> taskItems = new List<ITaskItem>();
+        Directory.GetParent(AppContext.BaseDirectory)!.GetFiles("*.json", SearchOption.AllDirectories).ToList().ForEach(file =>
+        {
+            taskItems.Add(new TaskItem(file.FullName));
+        });
+        JsonFileItems = taskItems.ToArray();
     }
 
 
     [Fact]
     public void Test1()
-    {
-        var item = new Mock<ITaskItem>();
-        item.Setup(x => x.GetMetadata("FullPath")).Returns("appsettings.json");
-        MergeJson mergeJson = new() { OutputPath=Path.Combine("TestGenerate",""),MainJsonFiles = "appsettings.json", JsonFileItems = new[] { item.Object } };
+    {       
+        MergeJson mergeJson = new() { OutputPath = "TestGenerate\\", MainJsonFiles = MainJsonFiles, JsonFileItems = JsonFileItems };
         mergeJson.BuildEngine = buildEngine.Object;
 
         bool success = mergeJson.Execute();
 
 
         Assert.True(success);
+
+        Assert.Equal(34, mergeJson.ParticipateMergeJsonFiles.Length);
     }
 }
