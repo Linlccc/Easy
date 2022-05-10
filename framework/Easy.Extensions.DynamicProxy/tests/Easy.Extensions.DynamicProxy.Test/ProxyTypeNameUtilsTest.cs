3b057106205d02d23434549656f431db7dbdb705
Linlccc <1610450962@qq.com>
2022-05-10 18:38:05 +0800

    动态代理测试单元

---
+++ framework/Easy.Extensions.DynamicProxy/tests/Easy.Extensions.DynamicProxy.Test/ProxyTypeNameUtilsTest.cs
@@ -0,0 +1,15 @@
+﻿using Easy.Extensions.DynamicProxy.Utils;
+using Xunit;
+
+namespace Easy.Extensions.DynamicProxy.Testl;
+
+public class ProxyTypeNameUtilsTest
+{
+
+    [Fact]
+    public void GetProxyTypeName()
+    {
+        ProxyTypeNameUtils proxyTypeNameUtils = new ProxyTypeNameUtils();
+        string n = proxyTypeNameUtils.GetProxyTypeName(typeof(string), typeof(int));
+    }
+}
