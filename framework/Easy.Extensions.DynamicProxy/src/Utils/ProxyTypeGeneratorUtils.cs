e7971365e6cfcbb364e708a67cb9306e4a47397e
Linlccc <1610450962@qq.com>
2022-05-10 18:38:29 +0800

    动态代理（很乱）

--- framework/Easy.Extensions.DynamicProxy/src/Utils/ProxyTypeGeneratorUtils.cs
+++ framework/Easy.Extensions.DynamicProxy/src/Utils/ProxyTypeGeneratorUtils.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Globalization;
+using System.Reflection;
 using System.Reflection.Emit;
 
 namespace Easy.Extensions.DynamicProxy.Utils;
@@ -11,7 +12,7 @@ internal class ProxyTypeGeneratorUtils
     /// <summary>
     /// 代理类型程序集名称
     /// </summary>
-    private const string ProxyAssemblyName = "Easy.DynamicProxy.Generate";
+    private const string ProxyAssemblyName = "Easy.DynamicProxy.Generator";
     /// <summary>
     /// 模块构建器
     /// </summary>
@@ -30,9 +31,198 @@ internal class ProxyTypeGeneratorUtils
 
         TypeBuilder typeBuild = moduleBuilder.DefineType(typeName, TypeAttributes.Public | TypeAttributes.Class | TypeAttributes.Sealed, classType, new[] { interfaceType });
 
+        DefineGenericParameter(typeBuild, interfaceType);
+
+        // 字段
+        FieldBuilder fieldBuilder1 = typeBuild.DefineField("_instance", typeof(IInterface), FieldAttributes.Private);
+        FieldBuilder _target = typeBuild.DefineField("_target", interfaceType, FieldAttributes.Private);
+
+        // 构造函数
+        ConstructorBuilder constructorBuilder = typeBuild.DefineConstructor(MethodAttributes.Public, typeof(object).GetTypeInfo().DeclaredConstructors.Single().CallingConvention, new Type[] { typeof(IInterface), interfaceType });
+        constructorBuilder.DefineParameter(1, ParameterAttributes.None, "_instance");
+        constructorBuilder.DefineParameter(2, ParameterAttributes.None, "_target");
+
+        ILGenerator iLGenerator = constructorBuilder.GetILGenerator();
+        iLGenerator.Emit(OpCodes.Ldarg_0);
+        iLGenerator.Emit(OpCodes.Call, typeof(object).GetTypeInfo().DeclaredConstructors.Single());
+
+        iLGenerator.Emit(OpCodes.Ldarg_0);
+        iLGenerator.Emit(OpCodes.Ldarg_1);
+        iLGenerator.Emit(OpCodes.Stfld, fieldBuilder1);
+
+        iLGenerator.Emit(OpCodes.Ldarg_0);
+        iLGenerator.Emit(OpCodes.Ldarg_2);
+        iLGenerator.Emit(OpCodes.Stfld, _target);
+
+        iLGenerator.Emit(OpCodes.Ret);
+
+        // 方法
+        // 绑定属性的方法
+        List<MethodInfo?> bindMethodOfProperty = interfaceType.GetProperties().Where(p => p.CanRead || p.CanWrite).Select(p => p.CanRead ? p.GetMethod : p.SetMethod).ToList();
+        List<MethodInfo> methodInfos = interfaceType.GetTypeInfo().DeclaredMethods.Where(m => !bindMethodOfProperty.Contains(m)).ToList();
+        foreach (MethodInfo methodInfo in methodInfos)
+        {
+
+        }
+
 
 
         return Type.GetType("")!;
+    }
+
+    private static MethodBuilder DefineMethod(MethodInfo methodInfo, MethodAttributes methodAttributes, Type classType, TypeBuilder typeBuild)
+    {
+        MethodBuilder methodBuilder = typeBuild.DefineMethod(methodInfo.Name, MethodAttributes.Public | MethodAttributes.Final | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.Virtual, methodInfo.CallingConvention, methodInfo.ReflectedType, methodInfo.GetParameters().Select(p => p.ParameterType).ToArray());
+
+        // 继承目标方法的特性
+        foreach (var item in methodInfo.CustomAttributes)
+        {
+            methodBuilder.SetCustomAttribute(DefineCustomAttribute(item));
+        }
+
+        DefineParameters(methodInfo, methodBuilder);
+
+        //MethodInfo methodInfo1 = classType.
+
+        return methodBuilder;
+    }
+
+    //private static MethodInfo GetMethodBySignature(Type type, MethodInfo method)
+    //{
+    //    var methods = type.GetTypeInfo().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+
+    //}
+
+
+
+    private static void DefineParameters(MethodInfo methodInfo, MethodBuilder methodBuilder)
+    {
+        ParameterInfo[] parameterInfos = methodInfo.GetParameters();
+        if (parameterInfos.Length > 0)
+        {
+            foreach (var item in parameterInfos)
+            {
+                ParameterBuilder parameterBuilder = methodBuilder.DefineParameter(item.Position + 1, item.Attributes, item.Name);
+                if ((item.Attributes & ParameterAttributes.HasDefault) != 0)
+                {
+                    try
+                    {
+                        object? defaultValue;
+                        try
+                        {
+                            defaultValue = item.DefaultValue;
+                        }
+                        catch (FormatException) when (item.ParameterType == typeof(DateTime))
+                        {
+                            defaultValue = null;
+                        }
+                        catch (FormatException) when (item.ParameterType.IsEnum)
+                        {
+                            defaultValue = null;
+                        }
+
+                        if (defaultValue is Missing) return;
+
+                        try
+                        {
+                            parameterBuilder.SetConstant(defaultValue);
+                        }
+                        catch (ArgumentException)
+                        {
+                            Type parameterType = item.ParameterType;
+                            Type parameterNonNullType = parameterType;
+                            if (defaultValue == null)
+                            {
+                                if (parameterType.GetTypeDefinition() == typeof(Nullable<>) || parameterType.IsValueType) return;
+                            }
+                            else if (parameterType.GetTypeDefinition() == typeof(Nullable<>))
+                            {
+                                parameterNonNullType = parameterType.GenericTypeArguments[0];
+                                if (parameterNonNullType.IsEnum || parameterNonNullType.IsInstanceOfType(defaultValue)) return;
+                            }
+                            try
+                            {
+                                var coercedDefaultValue = Convert.ChangeType(defaultValue, parameterNonNullType, CultureInfo.InvariantCulture);
+                                parameterBuilder.SetConstant(coercedDefaultValue);
+
+                                return;
+                            }
+                            catch
+                            {
+
+
+                            }
+                            throw;
+                        }
+                    }
+                    catch
+                    {
+                    }
+                }
+
+                foreach (var ca in item.CustomAttributes)
+                {
+                    parameterBuilder.SetCustomAttribute(DefineCustomAttribute(ca));
+                }
+            }
+        }
+
+        ParameterInfo returnParameterInfo = methodInfo.ReturnParameter;
+        var returnParameterBuilder = methodBuilder.DefineParameter(0, returnParameterInfo.Attributes, returnParameterInfo.Name);
+        foreach (var attribute in returnParameterInfo.CustomAttributes)
+        {
+            returnParameterBuilder.SetCustomAttribute(DefineCustomAttribute(attribute));
+        }
+    }
+
+    public static CustomAttributeBuilder DefineCustomAttribute(CustomAttributeData customAttributeData)
+    {
+        if (customAttributeData.NamedArguments != null)
+        {
+            var attributeTypeInfo = customAttributeData.AttributeType.GetTypeInfo();
+            var constructor = customAttributeData.Constructor;
+            //var constructorArgs = customAttributeData.ConstructorArguments.Select(c => c.Value).ToArray();
+            var constructorArgs = customAttributeData.ConstructorArguments
+                .Select(ReadAttributeValue)
+                .ToArray();
+            var namedProperties = customAttributeData.NamedArguments
+                    .Where(n => !n.IsField)
+                    .Select(n => attributeTypeInfo.GetProperty(n.MemberName))
+                    .ToArray();
+            var propertyValues = customAttributeData.NamedArguments
+                     .Where(n => !n.IsField)
+                     .Select(n => ReadAttributeValue(n.TypedValue))
+                     .ToArray();
+            var namedFields = customAttributeData.NamedArguments.Where(n => n.IsField)
+                     .Select(n => attributeTypeInfo.GetField(n.MemberName))
+                     .ToArray();
+            var fieldValues = customAttributeData.NamedArguments.Where(n => n.IsField)
+                     .Select(n => ReadAttributeValue(n.TypedValue))
+                     .ToArray();
+            return new CustomAttributeBuilder(customAttributeData.Constructor, constructorArgs
+               , namedProperties!
+               , propertyValues, namedFields!, fieldValues);
+        }
+        else
+        {
+            return new CustomAttributeBuilder(customAttributeData.Constructor, customAttributeData.ConstructorArguments.Select(c => c.Value).ToArray());
+        }
+    }
+
+    private static object ReadAttributeValue(CustomAttributeTypedArgument argument)
+    {
+        var value = argument.Value;
+        if (argument.ArgumentType.GetTypeInfo().IsArray == false)
+        {
+            return value!;
+        }
+        //special case for handling arrays in attributes
+        //the actual type of "value" is ReadOnlyCollection<CustomAttributeTypedArgument>.
+        var arguments = ((IEnumerable<CustomAttributeTypedArgument>)value!)
+            .Select(m => m.Value)
+            .ToArray();
+        return arguments;
     }
 
 
@@ -41,12 +231,12 @@ internal class ProxyTypeGeneratorUtils
     /// </summary>
     /// <param name="typeBuilder">类型构造器</param>
     /// <param name="targetType">目标类型</param>
-    private static void DefineGenericParameter(TypeBuilder typeBuilder,Type targetType)
+    private static void DefineGenericParameter(TypeBuilder typeBuilder, Type targetType)
     {
         if (!targetType.IsGenericTypeDefinition) return;
 
         Type[] genericArgumentTypes = targetType.GetGenericArguments();
-        GenericTypeParameterBuilder[] genericTypeParameterBuilders = typeBuilder.DefineGenericParameters(genericArgumentTypes.Select(gat=>gat.Name).ToArray());
+        GenericTypeParameterBuilder[] genericTypeParameterBuilders = typeBuilder.DefineGenericParameters(genericArgumentTypes.Select(gat => gat.Name).ToArray());
         // 配置泛型参数的约束信息
         foreach (GenericTypeParameterBuilder genericTypeParameterBuilder in genericTypeParameterBuilders)
         {
@@ -60,5 +250,10 @@ internal class ProxyTypeGeneratorUtils
         }
 
     }
+}
+
+public interface IInterface
+{
+    void Invoke();
 }
 
---
+++ framework/Easy.Extensions.DynamicProxy/src/Utils/ProxyTypeNameUtils.cs
@@ -0,0 +1,39 @@
+﻿namespace Easy.Extensions.DynamicProxy.Utils;
+
+/// <summary>
+/// 代理类型名称实用程序
+/// </summary>
+public class ProxyTypeNameUtils
+{
+    /// <summary>
+    /// 代理类型命名空间
+    /// </summary>
+    private const string ProxyTypeNamespace = "Easy.DynamicProxy.Generate";
+    /// <summary>
+    /// 代理类型名称索引
+    /// </summary>
+    private readonly Dictionary<string, int> _proxyTypeNameIndex;
+    /// <summary>
+    /// 代理类型名称地图
+    /// </summary>
+    private readonly Dictionary<Tuple<Type,Type>,string> _proxyTypeNameMap;
+
+    public ProxyTypeNameUtils()
+    {
+        _proxyTypeNameIndex = new();
+        _proxyTypeNameMap = new();
+    }
+
+    public string GetProxyTypeName(Type serviceType,Type implementationType)
+    {
+        Tuple<Type,Type> mapKey = Tuple.Create(serviceType,implementationType);
+        if(_proxyTypeNameMap.TryGetValue(mapKey, out string? proxyTypeName))return proxyTypeName;
+
+        proxyTypeName = $"{ProxyTypeNamespace}.{serviceType.Name()}";
+        if (!_proxyTypeNameIndex.TryGetValue(proxyTypeName, out int index)) _proxyTypeNameIndex[proxyTypeName] = 0;
+        proxyTypeName += $"_{Interlocked.Increment(ref index)}";
+
+        _proxyTypeNameMap[mapKey] = proxyTypeName;
+        return proxyTypeName;
+    }
+}
