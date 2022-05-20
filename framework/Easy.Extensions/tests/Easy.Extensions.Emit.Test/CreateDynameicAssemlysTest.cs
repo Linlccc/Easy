03c487f6b3f32523ba8c6ae1c145a019330b2504
Linlccc <1610450962@qq.com>
2022-05-21 01:12:14 +0800

    动态创建一个helloworld程序集

---
+++ framework/Easy.Extensions/tests/Easy.Extensions.Emit.Test/CreateDynameicAssemlysTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Easy.Extensions.Emit.Test
+{
+    public class CreateDynameicAssemlysTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            HelloWorld.Create();
+        }
+    }
+}
--- framework/Easy.Extensions/tests/Easy.Extensions.Emit.Test/GeneratorDynamicAssemlys/HelloWorld.cs
+++ framework/Easy.Extensions/tests/Easy.Extensions.Emit.Test/GeneratorDynamicAssemlys/HelloWorld.cs
@@ -2,33 +2,25 @@
 
 public class HelloWorld
 {
+    public static string _assemblyName = nameof(HelloWorld);
+
+    public static AssemblyBuilder _assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(_assemblyName), AssemblyBuilderAccess.RunAndSave);
+
+    public static ModuleBuilder _moduleBuilder = _assemblyBuilder.DefineDynamicModule(_assemblyName, $"{_assemblyName}.dll");
+
+
     public static void Create()
     {
-        var assemblyName = "HelloWorld";
-        var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(assemblyName), AssemblyBuilderAccess.Run);
-        var moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName);
-        var typeBuilder = moduleBuilder.DefineType("HelloWorld", TypeAttributes.Public);
+        var typeBuilder = _moduleBuilder.DefineType("HelloWorld", TypeAttributes.Public);
+        
         var methodBuilder = typeBuilder.DefineMethod("Main", MethodAttributes.Public | MethodAttributes.Static, typeof(void), new Type[] { });
         var il = methodBuilder.GetILGenerator();
         il.Emit(OpCodes.Ldstr, "Hello World!");
         il.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine", new Type[] { typeof(string) }));
         il.Emit(OpCodes.Ret);
-        var type = typeBuilder.CreateType();
-        assemblyBuilder.Save(assemblyName + ".dll");
-    }
 
-    public static void GeneratorDynamicAssemly1()
-    {
-        var assemblyName = new AssemblyName("DynamicAssembly1");
-        var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndSave);
-        var moduleBuilder = assemblyBuilder.DefineDynamicModule("DynamicModule1", "DynamicAssembly1.dll");
-        var typeBuilder = moduleBuilder.DefineType("DynamicType1", TypeAttributes.Public);
-        var methodBuilder = typeBuilder.DefineMethod("DynamicMethod1", MethodAttributes.Public | MethodAttributes.Static, typeof(void), new Type[] { typeof(string) });
-        var il = methodBuilder.GetILGenerator();
-        il.Emit(OpCodes.Ldstr, "Hello, World!");
-        il.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine", new Type[] { typeof(string) }));
-        il.Emit(OpCodes.Ret);
         typeBuilder.CreateType();
-        assemblyBuilder.Save("F:\\DynamicAssembly1.dll");
+
+        _assemblyBuilder.Save($"{_assemblyName}.dll");
     }
 }
