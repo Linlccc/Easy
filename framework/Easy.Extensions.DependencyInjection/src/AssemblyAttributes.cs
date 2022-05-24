a33776c81979e34405d14ee9b7be7ee176e0ae74
Linlccc <1610450962@qq.com>
2022-05-24 18:28:11 +0800

    添加程序集特性

---
+++ framework/Easy.Extensions.DependencyInjection/src/AssemblyAttributes.cs
@@ -0,0 +1,4 @@
+﻿// 程序集特性集
+
+// 仅对当前程序集可见的类对Easy.Extensions.DependencyInjection.Test程序集也可见
+[assembly: global::System.Runtime.CompilerServices.InternalsVisibleTo("Easy.Extensions.DependencyInjection.Test")]
