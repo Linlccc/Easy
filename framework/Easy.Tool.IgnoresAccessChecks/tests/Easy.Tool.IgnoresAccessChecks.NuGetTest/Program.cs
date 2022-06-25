32c3b126989df8c489ddbf4b3bda84325ae94a89
Linlccc <1610450962@qq.com>
2022-06-25 15:40:28 +0800

    添加 E.T.I 项目的NuGet测试项目

---
+++ framework/Easy.Tool.IgnoresAccessChecks/tests/Easy.Tool.IgnoresAccessChecks.NuGetTest/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.ServiceLookup;
+
+Console.WriteLine("使用DI的内部类型");
+ServiceProvider service = new ServiceCollection().BuildServiceProvider();
+ServiceProviderEngineScope root = service.Root;
+Console.WriteLine($"这里直接在访问不可访问的 {nameof(ServiceProviderEngineScope)} 类型,是否时root：{root.IsRootScope}");
+Console.WriteLine($"这里直接在访问不可访问的 {nameof(ServiceProviderEngineScope)} 类型,是否时root：{new ServiceProviderEngineScope(service, false).IsRootScope}");
+Console.ReadLine();
