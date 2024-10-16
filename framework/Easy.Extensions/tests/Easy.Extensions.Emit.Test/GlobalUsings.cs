aa7a72ed96f532e3444d484c16f18d7698d23495
Linlccc <linlccc@163.com>
2024-10-16 10:45:25 +0800

    修改 using 文件

--- framework/Easy.Extensions/tests/Easy.Extensions.Emit.Test/GlobalUsings.cs
+++ framework/Easy.Extensions/tests/Easy.Extensions.Emit.Test/GlobalUsings.cs
@@ -1,6 +1,4 @@
 ﻿global using System.Reflection;
 global using System.Reflection.Emit;
-
-global using Xunit;
-
 global using Easy.Extensions.Emit.Test.GeneratorDynamicAssemlys;
+global using Xunit;
