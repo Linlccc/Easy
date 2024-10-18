78849f72ef64c0cfcceaa8ecd475d59b3e682d49
Linlccc <linlccc@163.com>
2024-10-18 13:59:17 +0800

    PropertyInfo 扩展整理完成

---
+++ framework/Easy.Extensions/src/System.Reflection/PropertyInfoExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace System.Reflection;
+
+/// <summary>
+/// <see cref="PropertyInfo"/> 扩展
+/// </summary>
+public static class PropertyInfoExtensions
+{
+    /// <summary>
+    /// 设置指定对象的属性值。
+    /// </summary>
+    /// <param name="propertyInfo">要设置值的属性信息。</param>
+    /// <param name="obj">要设置属性值的对象。</param>
+    /// <param name="value">要设置的值。</param>
+    /// <exception cref="ArgumentNullException">如果 <paramref name="propertyInfo"/> 或 <paramref name="obj"/> 为 <c>null</c>。</exception>
+    public static void SetPropertyValue(this PropertyInfo propertyInfo, object? obj, object? value)
+    {
+        _ = propertyInfo ?? throw new ArgumentNullException(nameof(propertyInfo));
+        _ = obj ?? throw new ArgumentNullException(nameof(obj));
+
+        // 如果有 Set 方法，直接使用 SetValue 方法；否则使用字段赋值
+        if (propertyInfo.SetMethod is not null) propertyInfo.SetValue(obj, value);
+        else obj.GetType().GetField($"<{propertyInfo.Name}>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)?.SetValue(obj, value);
+    }
+}
---
+++ framework/Easy.Extensions/tests/Easy.Extensions.Test/System.Reflection/PropertyInfoExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace Easy.Extensions.Test.System.Reflection;
+
+public class PropertyInfoExtensions
+{
+    [Fact]
+    public void SetPropertyValue()
+    {
+        PropertyInfoTest test = new();
+        Type type = test.GetType();
+
+        PropertyInfo propNum = type.GetProperty(nameof(PropertyInfoTest.Num))!;
+        Assert.NotNull(propNum);
+        Assert.Equal(0, propNum.GetValue(test));
+        propNum.SetPropertyValue(test, 10);
+        Assert.Equal(10, propNum.GetValue(test));
+
+        PropertyInfo propI = type.GetProperty(nameof(PropertyInfoTest.I))!;
+        Assert.NotNull(propI);
+        Assert.Equal(0, propI.GetValue(test));
+        propI.SetPropertyValue(test, 100);
+        Assert.Equal(100, propI.GetValue(test));
+    }
+}
+
+public class PropertyInfoTest
+{
+    public int I { get; }
+
+    public int Num { get; set; }
+}
